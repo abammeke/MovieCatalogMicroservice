75b6f4ce44b882a0e971557711614a55ae70592c
abammeke <44130985+abammeke@users.noreply.github.com>
2019-05-06 08:51:07 -0500

    movie rating service

---
+++ movie-rating/src/main/java/com/xyxmovies/movierating/controller/RatingResource.java
@@ -0,0 +1,16 @@
+package com.xyxmovies.movierating.controller;
+
+import com.xyxmovies.movierating.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 10);
+    }
+}
